a280ab8d69bb805f72119cab15a1f9c444644ada
Niklas Larsson <niklaslarsson95@gmail.com>
2018-03-12 17:50:17 +1100

    Download a webpage and get access to that data

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 extern crate curl;
+extern crate html5ever;
 
 use std::io::{stdout, Write};
+use curl::easy::{Easy2, Handler, WriteError};
 
 use curl::easy;
 fn main() {
     println!("Hello, world!");
 
-    let mut curl = easy::Easy::new();
+
+    let mut curl = Easy2::new(Collector(Vec::new()));
+    curl.get(true).unwrap();
     curl.url("https://en.wikipedia.org/wiki/Nail").unwrap();
-    curl.write_function(|data| {
-        Ok(stdout().write(data).unwrap())
-    });
     curl.perform().unwrap();
+
+    let web = curl.get_ref();
+
+    println!("{}", String::from_utf8_lossy(&web.0));
 }
+
+struct Collector(Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0.extend_from_slice(data);
+        Ok(data.len())
+    }
+}
+
