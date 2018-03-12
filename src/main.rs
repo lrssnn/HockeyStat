c1f6bd515bb1deff3b97985609f4bbd9c9400e55
Niklas Larsson <niklaslarsson95@gmail.com>
2018-03-12 21:59:53 +1100

    Toy html parse

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 extern crate curl;
-extern crate html5ever;
+#[macro_use] extern crate html5ever;
 
-use std::io::{stdout, Write};
+use std::io::{stdout, Write, Read};
+use std::iter::repeat;
 use curl::easy::{Easy2, Handler, WriteError};
+
+use html5ever::parse_document;
+use html5ever::rcdom::{NodeData, RcDom, Handle};
+use html5ever::tendril::TendrilSink;
 
 use curl::easy;
 fn main() {
@@ -16,8 +21,14 @@ fn main() {
 
     let web = curl.get_ref();
 
-    println!("{}", String::from_utf8_lossy(&web.0));
+    let dom = parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut web.0.as_slice())
+        .unwrap();
+
+    walk(0, dom.document);
 }
+
 
 struct Collector(Vec<u8>);
 
@@ -28,3 +39,48 @@ impl Handler for Collector {
     }
 }
 
+fn walk(indent: usize, handle: Handle) {
+    let node = handle;
+
+    print!("{}", repeat(" ").take(indent).collect::<String>());
+
+    let mut desired = true;
+
+    match node.data {
+        NodeData::Document
+            => println!("#Document"),
+
+        NodeData::Doctype { ref name, ref public_id, ref system_id }
+            => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
+
+        NodeData::Text { ref contents }
+            => println!("#text: {}", escape_default(&contents.borrow())),
+
+        NodeData::Element {ref name, ref attrs, ..} => {
+            assert!(name.ns == ns!(html));
+            if name.local == local_name!("script")  { desired = false; }
+            else {
+                print!("<{}", name.local);
+                for attr in attrs.borrow().iter() {
+                    assert!(attr.name.ns == ns!());
+                    print!(" {}=\"{}\"", attr.name.local, attr.value);
+                }
+                println!(">");
+            }
+        },
+
+        NodeData::ProcessingInstruction { .. } => unreachable!(),
+
+        NodeData::Comment{..} => ()
+    }
+
+    if !desired { return; }
+
+    for child in node.children.borrow().iter() {
+        walk(indent + 4, child.clone());
+    }
+}
+
+pub fn escape_default(s: &str) -> String {
+    s.chars().flat_map(|c| c.escape_default()).collect()
+}
