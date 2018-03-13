bfe3af637f89fa9d00d0fd3fdb6e631a3f9fe30f
Niklas Larsson <niklaslarsson95@gmail.com>
2018-03-13 11:39:18 +1100

    Use stats api and deserialize json

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,41 @@
 extern crate curl;
-#[macro_use] extern crate html5ever;
+extern crate serde_json;
 
 use std::io::{stdout, Write, Read};
 use std::iter::repeat;
 use curl::easy::{Easy2, Handler, WriteError};
 
-use html5ever::parse_document;
-use html5ever::rcdom::{NodeData, RcDom, Handle};
-use html5ever::tendril::TendrilSink;
+use serde_json::{Value, Error};
 
-use curl::easy;
 fn main() {
     println!("Hello, world!");
 
 
     let mut curl = Easy2::new(Collector(Vec::new()));
     curl.get(true).unwrap();
-    curl.url("https://en.wikipedia.org/wiki/Nail").unwrap();
+	curl.useragent("Chrome/41.0.2227.0").unwrap();
+    curl.url("https://statsapi.web.nhl.com/api/v1/schedule?startDate=2017-12-12&endDate=2017-12-12").unwrap();
     curl.perform().unwrap();
 
     let web = curl.get_ref();
 
+    let json = String::from_utf8(web.0.as_slice().to_vec()).unwrap();
+
+	println!("{}", json);
+
+    let v: Value = serde_json::from_str(&json).unwrap();
+
+    println!("{}", v["copyright"]);
+
+/*
     let dom = parse_document(RcDom::default(), Default::default())
         .from_utf8()
         .read_from(&mut web.0.as_slice())
         .unwrap();
 
+
     walk(0, dom.document);
+*/
 }
 
 
@@ -37,50 +46,4 @@ impl Handler for Collector {
         self.0.extend_from_slice(data);
         Ok(data.len())
     }
-}
-
-fn walk(indent: usize, handle: Handle) {
-    let node = handle;
-
-    print!("{}", repeat(" ").take(indent).collect::<String>());
-
-    let mut desired = true;
-
-    match node.data {
-        NodeData::Document
-            => println!("#Document"),
-
-        NodeData::Doctype { ref name, ref public_id, ref system_id }
-            => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
-
-        NodeData::Text { ref contents }
-            => println!("#text: {}", escape_default(&contents.borrow())),
-
-        NodeData::Element {ref name, ref attrs, ..} => {
-            assert!(name.ns == ns!(html));
-            if name.local == local_name!("script")  { desired = false; }
-            else {
-                print!("<{}", name.local);
-                for attr in attrs.borrow().iter() {
-                    assert!(attr.name.ns == ns!());
-                    print!(" {}=\"{}\"", attr.name.local, attr.value);
-                }
-                println!(">");
-            }
-        },
-
-        NodeData::ProcessingInstruction { .. } => unreachable!(),
-
-        NodeData::Comment{..} => ()
-    }
-
-    if !desired { return; }
-
-    for child in node.children.borrow().iter() {
-        walk(indent + 4, child.clone());
-    }
-}
-
-pub fn escape_default(s: &str) -> String {
-    s.chars().flat_map(|c| c.escape_default()).collect()
 }
